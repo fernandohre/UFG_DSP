1363c53b3301e6d1734785915e6c91fc14c0c265
Fernando Callata <fernandohre@hotmail.com>
2019-03-13 22:58:55 +0000

    adicionando arquivo no resources para ser lido e implementando leitura de arquivo

--- LeituraArquivo/src/main/java/br/ufg/inf/leituraarquivo/Principal.java
+++ LeituraArquivo/src/main/java/br/ufg/inf/leituraarquivo/Principal.java
@@ -1,11 +1,46 @@
 
 package br.ufg.inf.leituraarquivo;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author Fernando Callata
  */
 public class Principal {
     public static void main(String[] args) {
+        
+        String nomeArquivo = new File("src/main/resources/arquivo.txt")
+                                 .getAbsolutePath();
+        String linha = null;
+
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = new FileReader(nomeArquivo);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader leitorDeBuffer = new BufferedReader(fileReader);
+
+            while((linha = leitorDeBuffer.readLine()) != null) {
+                System.out.println(linha);
+            }   
+
+            // Always close files.
+            leitorDeBuffer.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Não foi possível abrir o arquivo '" + 
+                nomeArquivo + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Erro na leitura do arquivo '" 
+                + nomeArquivo + "'");
+        }
     }
 }
