f2d792d4fd6f50e8ecc229a4a9f3ea2c630b274a
Gilmar Ferreira Arantes <gilmar.arantes@goias.go.gov.br>
2019-03-14 08:28:55 -0300

    #31299 padronizando o repositório e o projeto.

---
+++ dsp20191/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+w
+Gsrc/main/java/br/com/fernandohre/dsp2019/aulas0104/ManipulaArquivo.java,0/b/0baa30886bea36d6efe1414a9bb5ff51907a4960
---
+++ dsp20191/src/main/java/br/com/fernandohre/dsp2019/aulas0104/ManipulaArquivo.java
@@ -0,0 +1,68 @@
+
+package br.com.fernandohre.dsp2019.aulas0104;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author Fernando Callata
+ */
+public class ManipulaArquivo {
+    public static void main(String[] args) {
+        
+        String nomeArquivo = new File("src/main/resources/arquivo.txt")
+                                 .getAbsolutePath();
+        escrevaArquivo("Disciplina para software persistente", nomeArquivo);
+        
+        exibaConteudoArquivo(nomeArquivo);
+    }
+    
+    private static void exibaConteudoArquivo(String nomeArquivoCaminho) {
+        String linha = null;
+
+        try {
+            FileReader leitorDeArquivo = new FileReader(nomeArquivoCaminho);
+
+            BufferedReader leitorDeBuffer = new BufferedReader(leitorDeArquivo);
+
+            while((linha = leitorDeBuffer.readLine()) != null) {
+                System.out.println(linha);
+            }
+            
+            leitorDeBuffer.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Não foi possível abrir o arquivo '" + 
+                nomeArquivoCaminho + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Erro na leitura do arquivo '" 
+                + nomeArquivoCaminho + "'");
+        }
+    }
+
+    private static void escrevaArquivo(String mensagem, 
+                                       String caminhoNomeArquivo) {
+        File arquivo = new File(caminhoNomeArquivo);
+        FileWriter fr = null;
+        try {
+            fr = new FileWriter(arquivo);
+            fr.write(mensagem);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                fr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
