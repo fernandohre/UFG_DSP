8c0f9f2b8c42429676d401777499e56c99aa81ac
Fernando Callata <fernandohre@hotmail.com>
2019-04-11 00:05:37 +0000

    adicionando método de menu e tela

--- dsp20191/src/main/java/br/com/fernandohre/dsp2019/aulas1316/ap/Cliente/Principal.java
+++ dsp20191/src/main/java/br/com/fernandohre/dsp2019/aulas1316/ap/Cliente/Principal.java
@@ -13,12 +13,17 @@ import br.com.fernandohre.dsp2019.aulas1316.ap.Persistencia.Instalacao.LotacaoCr
 import br.com.fernandohre.dsp2019.aulas1316.ap.Persistencia.PersistenciaJdbc;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Fernando Callata
  */
 public class Principal {
+    private static Scanner leitor = new Scanner(System.in);
+    
     public static void main(String args[]) throws Exception {
         System.out.println("Criando tabelas...");
         List<PersistenciaJdbc> tabelas = new ArrayList<PersistenciaJdbc>();
@@ -27,12 +32,28 @@ public class Principal {
         tabelas.add(new DepartamentoCreate());
         tabelas.add(new LotacaoCreate());
         
-        for(PersistenciaJdbc item : tabelas) {
-            item.crieTabela();
-        }
+        tabelas.forEach(conceito -> {
+            try {
+                conceito.crieTabela();
+            } catch(Exception e) {
+                System.out.println(e.getMessage());
+            }
+        });
     }
     
     public static void menu() {
-        System.out.println("");
+        System.out.println(" OPERAÇÕES ");
+        System.out.println(" 1 - Cadastrar");
+        System.out.println(" 2 - Atualizar");
+        System.out.println(" 3 - Consultar");
+    }
+    
+    public static void conceitos() {
+        System.out.println("Selecione o conceito : ");
+        System.out.println("1 - Funcionario");
+        System.out.println("2 - Departamento");
+        System.out.println("3 - Cargo");
+        System.out.println("4 - Lotacao");
+        System.out.println("5 - Voltar Tela de Operações");
     }
 }
