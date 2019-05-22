f64e839c6d4e7c2c5eb610d8a2bc0002655a4a70
Fernando Callata <fernandohre@hotmail.com>
2019-05-22 19:39:52 -0300

    adicionando atividade do dia 24/04/2019

---
+++ dsp20191/src/main/java/br/com/fernandohre/dsp2019/aula2528jpa/Veiculo.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fernandohre.dsp2019.aula2528jpa;
+
+/**
+ *
+ * @author aluno
+ */
+public class Veiculo {
+    private long id;
+    private String marca;
+    private String modelo;
+    private Integer anoFabricacao;
+    private Integer anoModelo;
+    private Integer potenciaMotor;
+    
+    public Veiculo(long id, String marca, String modelo,
+            Integer anoFabricacao, Integer anoModelo, Integer potenciaMotor) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anoFabricacao = anoFabricacao;
+        this.anoModelo = anoModelo;
+        this.potenciaMotor = potenciaMotor;
+    }
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Integer getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setAnoFabricacao(Integer anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    public Integer getAnoModelo() {
+        return anoModelo;
+    }
+
+    public void setAnoModelo(Integer anoModelo) {
+        this.anoModelo = anoModelo;
+    }
+
+    public Integer getPotenciaMotor() {
+        return potenciaMotor;
+    }
+
+    public void setPotenciaMotor(Integer potenciaMotor) {
+        this.potenciaMotor = potenciaMotor;
+    }
+}
